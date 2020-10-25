4234fc716a86af1dd7e0931ee5fa73308e833fee
Shad <farshadkalathingal@gmail.com>
2020-10-25 14:17:46 +0530

    First Update

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        center: true,
+        items:5,
+        loop:true,
+        margin:10,
+        autoplay:true,
+        autoplayTimeout:3000,
+        autoplayHoverPause:true,
+        autoplaySpeed: 5000,
+    })
+
+    $('#year').text(new Date().getFullYear());
+  });
