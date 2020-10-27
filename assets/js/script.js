2225d076dac3128bff21f0f6a4658b1d275429f2
Shad <farshadkalathingal@gmail.com>
2020-10-27 10:29:41 +0530

    Media query udated

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,24 +24,33 @@ $(document).ready(function(){
     });
 
     $('.bxslider').bxSlider({
-        minSlides: 5,
-		maxSlides: 10,
+        minSlides: 1,
+		maxSlides: 5,
         slideWidth: 257,
 		slideMargin: 17,
 		ticker: true,
 		tickerHover: true,
 		speed: 20000,
 		useCSS: false, 
-		infiniteLoop: false
+		infiniteLoop: false,
     });
 
     $('#featured .owl-carousel').owlCarousel({
         center: true,
-        items:3,
         loop:true,
         margin:10,
-        nav:true,
-        navText:["<i class='fas fa-chevron-left'></i>" , "<i class='fas fa-chevron-right'></i>"]
+        navText:["<i class='fas fa-chevron-left'></i>" , "<i class='fas fa-chevron-right'></i>"],
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            600:{
+                items:3,
+                nav:true
+            }
+        }
     });
 
     $('#movie .owl-carousel').owlCarousel({
